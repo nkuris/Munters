fc8adbaa677144d80587dd5660cd752d1fcaa3c9
nkuris <nkuris@gmail.com>
2026-08-19 19:26:13 +0300

    readme set

--- Munters.Server/obj/Debug/net10.0/Munters.Server.AssemblyInfo.cs
+++ Munters.Server/obj/Debug/net10.0/Munters.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Munters.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b9c62ef5d0be749ba82c7e0edf4867a91ef7a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b336362e422096bfdc0fb376d2ef391815e1aee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Munters.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Munters.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
